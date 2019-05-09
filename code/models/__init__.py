0c0a2489f47bc119572e90ebb06b7df5e8255ec2
kingjr <jeanremi.king@gmail.com>
2019-05-09 11:56:36 +0200

    fix import

--- code/models/__init__.py
+++ code/models/__init__.py
@@ -4,4 +4,4 @@ from .encoding import Encoding
 from .pls_kernel import KernelPLS
 from .cca_gallant import KernelCCA
 from .rrr import ReducedRankRegressor
-from .pretrained_regression import PretrainedRidgeCV
+from .pretrained_ridge import PretrainedRidgeCV
