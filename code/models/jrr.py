e0f04974734fbb2f9bd586ef65c3bbf1c3bd563c
kingjr <jeanremi.king@gmail.com>
2019-05-09 11:32:19 +0200

    incorporate sonquist

--- code/models/jrr.py
+++ code/models/jrr.py
@@ -21,13 +21,14 @@ class JRR(object):
     """
 
     def __init__(self, G=None, H=None, F=None, bagging=10, zero_off_diag=True,
-                 fit_encoding=True):
+                 fit_encoding=True, apply_sonquist=False):
         self.G = RidgeCV() if G is None else G
         self.H = RidgeCV() if H is None else H
         self.F = RidgeCV() if F is None else F
         self.bagging = bagging
         self.zero_off_diag = zero_off_diag
         self.fit_encoding = fit_encoding
+        self.apply_sonquist = apply_sonquist
 
     def fit(self, X, Y):
         if self.bagging in (0, False, None):
@@ -46,7 +47,10 @@ class JRR(object):
         # Estimate E
         self.E_ = np.mean(H, 0).T  # TODO: check transpose
         if self.zero_off_diag:
-            self.E_ = np.diag(np.diag(self.E_))
+            self.E_ = np.diag(self.E_)
+        if self.apply_sonquist:
+            self.E_ = sonquist_morgan(self.E_)
+        self.E_ = np.diag(self.E_)
         # Filter X by E hat and estimate F
         if self.fit_encoding:
             self.F.fit(X @ self.E_, Y)
@@ -59,6 +63,24 @@ class JRR(object):
         from sklearn.metrics import r2_score
         return r2_score(y, self.predict(X), sample_weight=sample_weight,
                         multioutput='variance_weighted')
+
+
+def sonquist_morgan(x):
+    z = np.sort(x)
+    n = z.size
+    m1 = 0
+    m2 = np.sum(z)
+    mx = 0
+    best = -1
+    for i in range(n-1):
+        m1 += z[i]
+        m2 -= z[i]
+        ind = (i+1)*(n-i-1)*(m1/(i+1)-m2/(n-i-1))**2
+        if ind > mx:
+            mx = ind
+            best = z[i]
+    # K = mx / (np.var(x)*n) # significance indicator, not used for now
+    return 1*(x > (best+1.0e-6))
 
 
 if __name__ == '__main__':
@@ -104,3 +126,11 @@ if __name__ == '__main__':
 
     print('E_auc', roc_auc_score(np.diag(E), np.diag(E_hat)))
     print('Y_score', score)
+
+    # compare extraction to E
+    jrr = JRR(apply_sonquist=True)
+    sonquist_score = jrr.fit(X[train], Y[train]).score(X[test], Y[test])
+    E_hat = jrr.E_
+    extraction_score = np.sum(abs(np.diag(E)-np.diag(E_hat)))
+    print('sonquist score', extraction_score)
+    print('sonquist Y_score', score)
