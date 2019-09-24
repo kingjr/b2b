b08d4d708bfc6bf277ee213abdbd08680fc4e0f8
Jean Remi KING <jeanremi.king@gmail.com>
2019-09-24 03:46:58 -0700

    iclr meg and synthetic update

--- code/meg_experiment/base.py
+++ code/meg_experiment/base.py
@@ -3,7 +3,10 @@ import numpy as np
 import os.path as op
 from scipy.linalg import svd
 from scipy.stats import pearsonr
+
+from sklearn.base import clone
 from sklearn.preprocessing import scale
+from sklearn.model_selection import ShuffleSplit
 from common import (add_part_of_speech, add_word_frequency, add_word_length,
                     read_log, get_log_times, data_path)
 
@@ -43,20 +46,27 @@ def fetch_data(log_file):
     df = add_word_frequency(epochs.metadata[['word', ]])
     df = add_word_length(df)
     df = add_part_of_speech(df)
+    df['word_func'] = False
+    df.loc[df.query('(ADP+CONJ+DET+PRON)==True').index,
+           'word_func'] = True
     word = df.word.apply(lambda s: s.lower())
     for letter in 'abcdefghijklmnopqrstuvwxyz':
         df[letter] = word.str.count(letter) > 1
+    
+    df['word_dummy'] = (scale(df['word_length']) + 
+                        scale(df['word_freq']) + 
+                        np.random.randn(len(df)))
 
     meg = epochs.get_data()
     n_samples, n_chans, n_times = meg.shape
     for t in range(n_times):
         meg[:, :, t] = scale(meg[:, :, t])
 
-    columns = ['word_freq', 'word_length']
-    names = [k for k in df.keys() if k not in ('word', 'pos')]
+    names = ['word_length', 'word_freq', 'word_func', 'word_dummy']
+
     features = scale(df[names].values)
 
-    return meg, epochs.times, features, names
+    return meg, epochs.times, features, names, df.word
 
 
 def ridge_cv(X, Y, alphas, independent_alphas=False, Uv=None):
@@ -110,14 +120,13 @@ def ridge_cv(X, Y, alphas, independent_alphas=False, Uv=None):
     return coefs, best_alphas, Y_hat
 
 
-def fit_knockout(X, Y, alphas, knockout=True, independent_alphas=False,
+def fit_knockout(X, Y, alphas, independent_alphas=False,
                  pairwise=False):
     """Fit model using all but knockout X features"""
     n_x, n_y = X.shape[1], Y.shape[1]
-    knockout = np.arange(n_x)[:, None] if knockout is True else knockout
 
     K = list()
-    for xi in knockout:
+    for xi in np.arange(n_x)[:, None]:
         not_xi = np.setdiff1d(np.arange(n_x), xi)
 
         # When X and Y are matched in dimension, we can speed things
@@ -150,25 +159,58 @@ def r_score(Y_true, Y_pred):
 
 class B2B():
     def __init__(self, alphas=np.logspace(-4, 4, 20),
-                 independent_alphas=True,
-                 knockout=False):
+                 independent_alphas=True, ensemble=None):
         self.alphas = alphas
-        self.knockout = knockout
         self.independent_alphas = independent_alphas
+        self.ensemble = ensemble
 
     def fit(self, X, Y):
-        # Fit decoder
-        self.G_, G_alpha, YG = ridge_cv(
-            Y, X, self.alphas, self.independent_alphas)
-        # Fit encoder
-        self.H_, H_alpha, _ = ridge_cv(X, YG, self.alphas,
-                                       self.independent_alphas)
-        # Fit knock-out encoders
-        if self.knockout:
-            self.Ks_ = fit_knockout(X, YG, self.alphas,
-                                    self.knockout,
-                                    self.independent_alphas, True)
+        
+        self.G_ = list()
+        self.H_ = list()
+        self.Ks_ = list()
+        
+        # Prepare ensembling
+        if self.ensemble is None:
+            ensemble = [(range(len(X)), range(len(X))),]
+        else:
+            if isinstance(self.ensemble, int):
+                ensemble = ShuffleSplit(self.ensemble)
+            else:
+                ensemble = self.ensemble
+            ensemble = [split for split in ensemble.split(X)]
+            
+        # Ensembling loop
+        for train, test in ensemble:
+            
+            # Fit decoder
+            G, G_alpha, YG = ridge_cv(Y[train], X[train], 
+                                      self.alphas, 
+                                      self.independent_alphas)
+            self.G_.append(G)
+            
+            if len(X[train]) != len(X):
+                YG = Y @ G.T
+            
+            # Fit encoder
+            H, H_alpha, _ = ridge_cv(X[test], YG[test],
+                                     self.alphas,
+                                     self.independent_alphas)
+            self.H_.append(H)
+            
+            # Fit knock-out encoders
+            Ks = fit_knockout(X, YG, 
+                              self.alphas,
+                              self.independent_alphas, True)
+            self.Ks_.append(Ks)
 
+        # Aggregate ensembling
+        self.G_ = np.mean(self.G_, 0)
+        self.H_ = np.mean(self.H_, 0)
+        self.Ks_ = np.mean(self.Ks_, 0)
+        
+        self.E_ = np.diag(self.H_)
+        
         return self
 
     def score(self, X, Y):
@@ -182,45 +224,32 @@ class B2B():
     def score_knockout(self, X, Y):
         # Transform with decoder
         YG = Y @ self.G_.T
-        # Allow knocking out blocks of features
-        if self.knockout is True:
-            knockout = range(X.shape[1])
-        else:
-            knockout = self.knockout
 
         # For each knock-out, compute R score
         K_scores = list()
-        for xi, K in zip(knockout, self.Ks_):
+        for xi, K in enumerate(self.Ks_):
             # Knocked-out encoder predictions
             XK = X @ K.T
             # R score for each relevant dimensions of X
-            r = r_score(YG[:, xi], XK[:, xi])
-            # If one dimension, make it float
-            if isinstance(xi, int):
-                r = r[0]
-            # Difference between standard and knocked-out scores
-            K_scores.append(r)
+            K_scores.append(r_score(YG[:, xi], XK[:, xi])[0])
 
         return np.array(K_scores)
 
 
 class Forward():
-    def __init__(self, alphas=np.logspace(-4, 4, 20), independent_alphas=True,
-                 knockout=True):
+    def __init__(self, alphas=np.logspace(-4, 4, 20), independent_alphas=True):
         self.alphas = alphas
         self.independent_alphas = independent_alphas
-        self.knockout = knockout
 
     def fit(self, X, Y):
         # Fit encoder
         self.H_, H_alpha, _ = ridge_cv(X, Y, self.alphas,
                                        self.independent_alphas)
         # Fit knock-out encoders
-        if self.knockout:
-            self.Ks_ = fit_knockout(X, Y, self.alphas,
-                                    self.knockout,
-                                    self.independent_alphas, False)
+        self.Ks_ = fit_knockout(X, Y, self.alphas,
+                                self.independent_alphas, False)
 
+        self.E_ = np.sum(self.H_**2, 0)
         return self
 
     def score(self, X, Y):
@@ -230,18 +259,107 @@ class Forward():
         return r_score(Y, XH)
 
     def score_knockout(self, X, Y):
-        if self.knockout is True:
-            knockout = range(X.shape[1])
-        else:
-            knockout = self.knockout
-
-        R = list()
-        for xi, K in zip(knockout, self.Ks_):
+        K_scores = list()
+        for xi, K in enumerate(self.Ks_):
             # Knocked-out encoder predictions
             XK = X @ K.T
             # R score for each relevant dimensions of X
             K_score = r_score(Y, XK)
             # Difference between standard and knocked-out scores
-            R.append(K_score)
+            K_scores.append(K_score)
 
-        return np.array(R)
+        return np.array(K_scores)
+    
+    def predict(self, X):
+        return X @ self.H_.T
+
+
+class Backward():
+    def __init__(self, alphas=np.logspace(-4, 4, 20), independent_alphas=True):
+        self.alphas = alphas
+        self.independent_alphas = independent_alphas
+
+    def fit(self, X, Y):
+        # Fit encoder
+        self.H_, H_alpha, _ = ridge_cv(Y, X, self.alphas,
+                                       self.independent_alphas)
+
+        self.E_ = np.sum(self.H_**2, 1)
+        return self
+
+    def score(self, X, Y):
+        # Make standard and knocked-out encoders predictions
+        YH = Y @ self.H_.T
+        # Compute R for each column of Y
+        return r_score(X, YH)
+
+    def score_knockout(self, X, Y):
+        return 0
+    
+    
+    def predidct(self, X):
+        return 0
+
+
+def cross_decomp_transform(model, X, Y):
+    
+        X = X - model.x_mean_
+        X /= model.x_std_
+        X = X @ model.x_rotations_
+        
+        Y = Y - model.y_mean_
+        Y /= model.y_std_
+        Y = Y @ model.y_rotations_
+        
+        return X, Y
+
+
+class CrossDecomp():
+    """fit X => Y"""
+    def __init__(self, model):
+        self.model = model
+
+    def fit(self, X, Y):
+        # fit CCA
+        self.model.fit(X, Y)
+        
+        if hasattr(self.model, 'estimator'):
+            self.model_ = self.model.estimator.fit(X, Y)
+        else:
+            self.model_ = self.model
+
+        # fit_knockout
+        n_x = X.shape[1]
+        self.Ks_ = list()
+        for xi in np.arange(n_x):
+            
+            knockout = np.diag(np.ones(n_x))
+            knockout[xi] = 0
+            self.Ks_.append(clone(self.model_).fit(X @ knockout, Y))
+        
+        self.E_ = np.sum(self.model_.x_rotations_**2, 1)
+        return self
+
+    def score(self, X, Y):
+        X, Y = cross_decomp_transform(self.model_, X, Y)
+        R_score = r_score(X, Y)
+        return R_score
+   
+    def score_knockout(self, X, Y):
+
+        n_x = X.shape[1]
+        
+        K_scores = list()
+        for xi, K in enumerate(self.Ks_):
+       
+            knockout = np.diag(np.ones(n_x))
+            knockout[xi] = 0
+            
+            Xk, Yk = cross_decomp_transform(K, X @ knockout, Y)
+
+            K_scores.append(r_score(Xk, Yk))
+        
+        return np.array(K_scores)
+    
+    def predict(self, X):
+        return self.model_.predict(X)
